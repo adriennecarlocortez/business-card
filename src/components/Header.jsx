34815534d115490f0c5464fd63f6d255c1635758
adriennecarlocortez <cortez.adrienne00@gmail.com>
2023-10-25 12:41:50 +0800

    Fixed links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,16 @@ export default function Header() {
             <h1 className="nav--name" >Adrienne Cortez</h1>
             <h3 className="nav--title" >Frontend Developer</h3>
             <h4 className="nav--site" >adrienne.website</h4>
-            <button className="nav--btn_email">Email</button>
-            <button className="nav--btn_in">LinkedIn</button>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=cortez.adrienne00@gmail.com" target="_blank">
+            <button className="nav--btn_email">
+                Email
+            </button>
+            </a>
+            <a href="https://www.linkedin.com/in/adriennecarlocortez/" target="_blank">
+                <button className="nav--btn_in">
+                    LinkedIn
+                </button>
+            </a>
         </nav>
     )
 }
